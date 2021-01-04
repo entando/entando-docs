e258b1327f098c40e00d4163244a6ea161be1771
nshaw <n.shaw@entando.com>
2021-01-04 11:00:27 -0500

    ENDOC-175 Add hubspot tracking to theme pages

---
+++ vuepress/docs/.vuepress/theme/layouts/Layout.vue
@@ -0,0 +1,19 @@
+<!-- Override the default Layout so we can add hubspot-tracking to all content pages using the theme.
+Similar code exists in components/SpecialLayout.vue -->
+<template>
+  <div>
+    <Layout/>
+    <hubspot-tracking/>
+  </div>
+</template>
+<script>
+import Layout from '@parent-theme/layouts/Layout.vue'
+import HubspotTracking from "../../components/HubspotTracking";
+
+export default {
+  components: {
+    HubspotTracking,
+    Layout
+  }
+}
+</script>
