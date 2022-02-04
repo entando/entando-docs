f04aec57b26d50c30ab07d49ca24422499315f23
Lydia <l.pedersen@entando.com>
2022-02-04 12:39:26 -0800

    ENDOC-416b Fix nav conflict

--- vuepress/docs/.vuepress/next.js
+++ vuepress/docs/.vuepress/next.js
@@ -84,12 +84,12 @@ module.exports = {
                         path: path + 'compose/welcome-wizard.md'
                     },
                     {
-                        title: 'Entando Component Manager',
-                        path: path  + 'compose/ecm-overview.md'
-                    },
-                    {
                         title: 'Entando Component Repository',
                         path: path  + 'compose/ecr-overview.md'
+                    },
+                    {
+                        title: 'Entando Component Manager',
+                        path: path  + 'compose/ecm-overview.md'
                     },
                 ]
             },
--- vuepress/docs/.vuepress/v632.js
+++ vuepress/docs/.vuepress/v632.js
@@ -80,12 +80,12 @@ module.exports = {
                         path: path + 'compose/welcome-wizard.md'
                     },
                     {
-                        title: 'Entando Component Manager',
-                        path: path  + 'compose/ecm-overview.md'
-                    },
-                    {
                         title: 'Entando Component Repository',
                         path: path  + 'compose/ecr-overview.md'
+                    },
+                    {
+                        title: 'Entando Component Manager',
+                        path: path  + 'compose/ecm-overview.md'
                     },
                 ]
             },
