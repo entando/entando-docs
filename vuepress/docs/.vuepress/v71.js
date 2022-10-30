a1254705f69880cdf1fa0a457b4585d67fe532aa
Nathan Shaw <n.shaw@entando.com>
2022-10-30 18:05:47 -0400

    ENDOC-540 Add API Claim tutorial

--- vuepress/docs/.vuepress/v71.js
+++ vuepress/docs/.vuepress/v71.js
@@ -337,6 +337,10 @@ module.exports = {
                                 path: path + 'create/ms/add-access-controls.md'
                             },
                             {
+                                title: 'Add an API Claim',
+                                path: path + 'create/ms/add-api-claim.md'
+                            },
+                            {
                                 title: 'Use Postman with OAuth2 APIs',
                                 path: path + 'create/ms/use-postman-with-oauth2.md'
                             },
