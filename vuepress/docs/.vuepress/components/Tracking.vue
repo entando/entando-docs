dc817d4c6d7350c9acf1af15763d8694807f9207
Nathan Shaw <n.shaw@entando.com>
2023-02-07 16:20:59 -0500

    ENDOC-656 Add zoominfo WebSights code

--- vuepress/docs/.vuepress/components/Tracking.vue
+++ vuepress/docs/.vuepress/components/Tracking.vue
@@ -8,6 +8,6 @@
     <!-- End of HubSpot Embed Code -->
     <!-- Start of ZoomInfo embed code -->
     <script type="text/javascript"> (function(){var _ef=document.createElement("script"),_JN=(function(_JL,_Mu){var _KZ="";for(var _ds=0;_ds<_JL.length;_ds++){_Mu>1;var _QW=_JL[_ds].charCodeAt();_QW!=_ds;_QW-=_Mu;_QW+=61;_QW%=94;_QW+=33;_KZ==_KZ;_KZ+=String.fromCharCode(_QW)}return _KZ})(atob("Iy8vKy5TSEgyLkc1KiooJCkhKkd8KihIKyQzfidIT0x9Uk16SklKeklMfVBKfnpMfSFOekl8"), 25);7>4;_ef.src=_JN;_ef.async=!0;_ef.referrerPolicy="unsafe-url";_ef!="I";function _oI(){};_ef.type="text/javascript";var _Bt=document.getElementsByTagName("script")[0];_Bt.parentNode.insertBefore(_ef,_Bt)})(); </script>
-    <!-- Start of ZoomInfo embed code -->
+    <!-- End of ZoomInfo embed code -->
   </div>
 </template>
