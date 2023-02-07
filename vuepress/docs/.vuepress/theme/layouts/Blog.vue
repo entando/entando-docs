71bbd76882e12d8fabc19667943563d8260fc23a
Anthony Viard <anth.viard@gmail.com>
2023-02-07 21:37:57 +0100

    [ENDOC-652] Add a dedicated template for blog entries including a Tag component

---
+++ vuepress/docs/.vuepress/theme/layouts/Blog.vue
@@ -0,0 +1,29 @@
+<!-- Override and extend the default theme Layout -->
+<template>
+  <div>
+    <Layout>
+      <template #sidebar-top>
+        <EntandoVersionLinks/>
+      </template>
+      <template #page-bottom>
+        <PostTag :tag="$frontmatter.tags"/>
+      </template>
+    </Layout>
+    <Tracking/>
+  </div>
+</template>
+<script>
+import Layout from '@parent-theme/layouts/Layout.vue'
+import EntandoVersionLinks from "../../components/EntandoVersionLinks";
+import Tracking from "../../components/Tracking";
+import PostTag from "../../components/PostTag";
+
+export default {
+  components: {
+    PostTag,
+    Layout,
+    EntandoVersionLinks,
+    Tracking
+  }
+}
+</script>
