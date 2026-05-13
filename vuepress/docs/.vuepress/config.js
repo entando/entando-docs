0af8e299ad60228231b35ee0629ebdd4a93ba9dc
entsimsan <sm.sanna@entando.com>
2026-05-13 12:54:04 +0200

    Keep API link in docs sidebar

--- vuepress/docs/.vuepress/config.js
+++ vuepress/docs/.vuepress/config.js
@@ -138,14 +138,12 @@ module.exports = {
       landingSecondaryNav: [
           {text: 'Docs', link: '/v7.5/docs/', target: '_self'},
           {text: 'Tutorials', link: '/v7.5/tutorials/', target: '_self'},
-          {text: 'API', link: '/v7.5/api/', target: '_self'},
           {text: 'Forum', link: 'https://forum.entando.com'},
 
       ],
       secondaryNav: [
           {text: 'Docs', link: 'javascript:Entando.versionedLink("/docs");', target: '_self'},
           {text: 'Tutorials', link: 'javascript:Entando.versionedLink("/tutorials");', target: '_self'},
-          {text: 'API', link: 'javascript:Entando.versionedLink("/api");', target: '_self'},
           {text: 'Forum', link: 'https://forum.entando.com'},
           
       ],
